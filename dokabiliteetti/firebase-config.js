c6efe2bd41bc914339ae80c5951018c74f0389f5
Tjaloz <github.wreckage656@passmail.com>
2026-09-02 18:13:36 +0300

    Add files via upload

--- dokabiliteetti/firebase-config.js
+++ dokabiliteetti/firebase-config.js
@@ -1,12 +1,12 @@
 // Korvaa alla olevat arvot omalla Firebase-projektisi asetuksilla.
 // Löydät nämä: Firebase Console -> Project settings -> Your apps -> SDK setup and configuration
 export const firebaseConfig = {
-  apiKey: "TÄYTÄ_TÄHÄN",
-  authDomain: "TÄYTÄ_TÄHÄN.firebaseapp.com",
-  projectId: "TÄYTÄ_TÄHÄN",
-  storageBucket: "TÄYTÄ_TÄHÄN.appspot.com",
-  messagingSenderId: "TÄYTÄ_TÄHÄN",
-  appId: "TÄYTÄ_TÄHÄN"
+  apiKey: "AIzaSyA7YOCVZ4bryHd2U4CN2iN-YEpKS0OZj_k",
+  authDomain: "dokabiliteetti-d8a10.firebaseapp.com",
+  projectId: "dokabiliteetti-d8a10",
+  storageBucket: "dokabiliteetti-d8a10.firebasestorage.app",
+  messagingSenderId: "849464191592",
+  appId: "1:849464191592:web:92334a56ca99760b3be9dd"
 };
 
 // Huom: nämä avaimet EIVÄT ole salaisia - Firebase-web-config on tarkoitettu
